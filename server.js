d2c8fd5c65a583ff8bc9b67b05e026bca4e297e4
Tien Vu <tienv2510@gmail.com>
2024-05-10 22:38:10 -0400

    Add simple session demo

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 import indexRouter from "./routes/index.js";
+import session from "express-session";
 
 // Constants
 const port = process.env.PORT || 3000;
@@ -23,7 +24,41 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join("public")));
 
-app.use("/", indexRouter);
+// Use the session middleware
+app.use(
+  session({
+    secret: "keyboard cat",
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// Access the session as req.session
+app.get("/", function (req, res, next) {
+  if (req.session.views) {
+    req.session.views++;
+    res.setHeader("Content-Type", "text/html");
+    res.write("<p>views: " + req.session.views + "</p>");
+    res.write("<p>expires in: " + req.session.cookie.maxAge / 1000 + "s</p>");
+    res.end();
+  } else {
+    req.session.views = 1;
+    res.end("welcome to the session demo. refresh!");
+  }
+});
+
+app.get("/print-sessions", (req, res) => {
+  const sessionStore = req.sessionStore;
+  sessionStore.all((error, sessions) => {
+    if (error) {
+      console.error("Failed to retrieve sessions:", error);
+      res.status(500).send("Failed to retrieve sessions.");
+      return;
+    }
+    res.send(sessions);
+  });
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
